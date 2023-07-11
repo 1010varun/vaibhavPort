f3c0f59b2831ee3a36726b8ef5c205e2cb5c6ca4
Varun Agarwal <varunagarwal10.2@gmail.com>
2023-07-12 01:00:44 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+function handelSubmit() {
+    const name = document.getElementById('custname').value;
+    const email = document.getElementById('custemail').value;
+    const message = document.getElementById('custcomment').value;
+
+    const params = {
+        name,
+        email,
+        message
+    }
+
+    const templateID = "template_8ujvmfg";
+    const serviceID = "service_ekvlkmd";
+
+    emailjs.send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("custname").value = "";
+            document.getElementById("custemail").value = "";
+            document.getElementById("custcomment").value = "";
+            alert("Message Send Successful:), Will contact super soon")
+        })
+        .catch((err) => {
+            alert("Error, Try Again Later!!")
+    })
+}
