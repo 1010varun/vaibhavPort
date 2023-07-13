62c4b4b7ece18b6db545c75b1c274c55b393f7a7
Varun Agarwal <varunagarwal10.2@gmail.com>
2023-07-13 21:01:37 +0530

    updated social links

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,23 +29,33 @@ function handelSubmit() {
     const email = document.getElementById('custemail').value;
     const message = document.getElementById('custcomment').value;
 
-    const params = {
-        name,
-        email,
-        message
+    if (name.length == 0 || email.length == 0 || message.length == 0) {
+        alert("Plaese Provide complete Details!! :|")
+        document.getElementById("custname").value = "";
+        document.getElementById("custemail").value = "";
+        document.getElementById("custcomment").value = "";
     }
 
-    const templateID = "template_8ujvmfg";
-    const serviceID = "service_ekvlkmd";
+    else {
 
-    emailjs.send(serviceID, templateID, params)
-        .then((res) => {
-            document.getElementById("custname").value = "";
-            document.getElementById("custemail").value = "";
-            document.getElementById("custcomment").value = "";
-            alert("Message Send Successful:), Will contact super soon")
-        })
-        .catch((err) => {
-            alert("Error, Try Again Later!!")
-    })
+        const params = {
+            name,
+            email,
+            message
+        }
+
+        const templateID = "template_8ujvmfg";
+        const serviceID = "service_ekvlkmd";
+
+        emailjs.send(serviceID, templateID, params)
+            .then((res) => {
+                document.getElementById("custname").value = "";
+                document.getElementById("custemail").value = "";
+                document.getElementById("custcomment").value = "";
+                alert("Message Send Successful:), Will contact super soon")
+            })
+            .catch((err) => {
+                alert("Error, Try Again Later!!")
+            })
+    }
 }
